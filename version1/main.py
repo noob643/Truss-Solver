4fe0bf94667930d271bdec819f38055d15c70160
noob643 <62906260+noob643@users.noreply.github.com>
2020-04-27 12:54:20 +0100

    Update main.py

--- version1/main.py
+++ version1/main.py
@@ -1,5 +1,6 @@
 #! python3
 #main.py - file that assembles all data, and performs calculation
+
 from definition import Node, Element, Support, Load
 from analysis_inputs import nodes_data, elements_data, supports_data, loadcases
 import numpy as np
@@ -20,7 +21,7 @@ for i in elements_data:
         elements_data[i][2], elements_data[i][3],
         elements_data[i][4],elements_data[i][5],
         elements_data[i][6]))
-            
+
 # Create supports
 supports=[]
 for i in supports_data:
@@ -58,23 +59,23 @@ st_mat_full=copy.deepcopy(st_mat)
 self_weight=[]
 for i in range(max(list(nodes_dict.keys()))*2):
     self_weight.append([0.0])
-    
-#Populate self weight matrix with element weights    
+
+#Populate self weight matrix with element weights
 for i in elements:
     self_weight[(i.sw[0]-1)*2][0]+=-i.sw[1]
     self_weight[(i.sw[0]-1)*2+1][0]+=-i.sw[2]
     self_weight[(i.sw[3]-1)*2][0]+=-i.sw[4]
     self_weight[(i.sw[3]-1)*2+1][0]+=-i.sw[5]
-    
+
 #Create loading vectors for each loadcase
 loadings = {}
 for i in list(loadcases.keys()):
-    
+
     loading=[]
-    
+
     for z in range(max(list(nodes_dict.keys()))*2):
         loading.append([0.0])
-    
+
     for z in loadcases[i]:
         loading[(int(z)-1)*2][0]+=float(loadcases[i][z][0])*1000 #multiply to convert from kN to N
         loading[(int(z)-1)*2+1][0]+=float(loadcases[i][z][1])*1000
@@ -87,42 +88,99 @@ for i in list(loadcases.keys()):
 support_nodes=[]
 for i in supports:
     support_nodes+=i.sup_cols_rows()
-loadings_full=copy.deepcopy(loadings['1'])
+    
 
-#Modify loading vectors and stiffness matrix based on support conditions
+#Modify stiffness matrix based on support conditions
 for i in list(reversed(support_nodes)):
     for z in range(len(st_mat)):
         del st_mat[z][i]
     del st_mat[i]
-    del loadings['1'][i]
+
+
+node_displacements={}
+support_reactions={}
+element_forces={}
+for l in loadings:
+
+#Define supported nodes
+    support_nodes=[]
+    for i in supports:
+        support_nodes+=i.sup_cols_rows()
     
+#Modify loading vectors based on support conditions
+    for i in list(reversed(support_nodes)):
+        del loadings[l][i]
+
 #Convert lists to matrices and find displacements
-A=np.array(st_mat)
-B=np.array(loadings['1'])
-disp=np.linalg.solve(A, B) #displacements
+    A=np.array(st_mat)
+    B=np.array(loadings[l])
+    disp=np.linalg.solve(A, B) #displacements
 
 #Insert missing support nodes
-disp = disp.tolist()
-for i in list(sorted(support_nodes)):
-    disp.insert((i),[0.0])
+    disp = disp.tolist()
+    for i in list(sorted(support_nodes)):
+        disp.insert((i),[0.0])
 
-#Record displacement against node instances    
-for i in nodes:
-    setattr(i,'dispx',disp[(i.name-1)*2][0])
-    setattr(i,'dispy',disp[(i.name-1)*2+1][0])
+#Record displacement against node instances
+
+    for i in nodes:
+        node_displacements.setdefault(l,{})
+        node_displacements[l].setdefault(i,[disp[(i.name-1)*2][0],disp[(i.name-1)*2+1][0]])
+    #setattr(i,'dispx',disp[(i.name-1)*2][0])
+    #setattr(i,'dispy',disp[(i.name-1)*2+1][0])
 
 #Calculate support reactions
-disp=np.array(disp)
-R=np.dot(st_mat_full, disp)
-R.tolist()
-for i in supports:
-    setattr(i,'rx',R[(i.point.name-1)*2][0])
-    setattr(i,'ry',R[(i.point.name-1)*2+1][0])
+    disp=np.array(disp)
+    R=np.dot(st_mat_full, disp)
+    R.tolist()
+    for i in supports:
+        support_reactions.setdefault(l,{})
+        support_reactions[l].setdefault(i,[R[(i.point.name-1)*2][0],R[(i.point.name-1)*2+1][0]])
+    #setattr(i,'rx',R[(i.point.name-1)*2][0])
+    #setattr(i,'ry',R[(i.point.name-1)*2+1][0])
 
 #Calculate force in elements
+    for i in elements:
+        lm=[i.k*i.cos, i.k*i.sin, i.k*(-i.cos), i.k*(-i.sin)]
+        disp1=[disp.tolist()[(i.startpos-1)*2],disp.tolist()[(i.startpos-1)*2+1],disp.tolist()[(i.endpos-1)*2],disp.tolist()[(i.endpos-1)*2+1]]
+        F=np.dot(np.array(lm),np.array(disp1))
+        element_forces.setdefault(l,{})
+        element_forces[l].setdefault(i, F[0])
+    #setattr(i,'force',F[0])
+
+# Assign displacements for node instances
+for j in nodes:
+    to_assign={}
+    for i in node_displacements:
+        to_assign.setdefault(i,node_displacements[i][j])
+        setattr(j,'disp',to_assign)
+
+for j in supports:
+    to_assign={}
+    for i in support_reactions:
+        to_assign.setdefault(i, support_reactions[i][j])
+        setattr(j,'res',to_assign)
+        
+for j in elements:
+    to_assign={}
+    for i in element_forces:
+        to_assign.setdefault(i,element_forces[i][j])
+        setattr(j,'force',to_assign)
+        
+results_file=open('results_file.txt','w')
+for i in nodes:
+    results_file.write('Node '+str(i.name)+'\n')
+    results_file.write('X: '+str(i.x)+' Y: '+str(i.y)+'\n\n')
+    for j in i.r():
+        results_file.write(j)
+
+for i in supports:
+    results_file.write(i.p())
+    for j in i.r():
+        results_file.write(j) 
+    
 for i in elements:
-    lm=[i.k*i.cos, i.k*i.sin, i.k*(-i.cos), i.k*(-i.sin)]
-    disp1=[disp.tolist()[(i.startpos-1)*2],disp.tolist()[(i.startpos-1)*2+1],disp.tolist()[(i.endpos-1)*2],disp.tolist()[(i.endpos-1)*2+1]]
-    F=np.dot(np.array(lm),np.array(disp1))
-    setattr(i,'force',F[0])
-  
+    results_file.write(i.p())
+    for j in i.r():
+        results_file.write(j)
+results_file.close()
